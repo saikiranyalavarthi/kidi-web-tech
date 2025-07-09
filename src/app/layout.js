60065258facc748a1ae30424e67e7110cef8d27d
saikiranyalavarthi <yalavarthisaikiran3482@gmail.com>
2025-07-09 10:37:29 +0530

    some blogs content add

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
         {children}
         <AnimatedServices />
         <Goals />
-        <AnimatedCompanies />
+        {/* <AnimatedCompanies /> */}
         <Footer />
       </body>
     </html>
